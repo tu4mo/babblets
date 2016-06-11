557d5740485d0be0e39b6406aa4a97e9c78ab0a3
tu4mo <tu4mo@users.noreply.github.com>
2016-06-11 19:23:27 +0300

    Use express to render home page

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,20 @@ var router = express.Router()
 
 var chat = require('../controllers/chat')
 
-router.get('/chat/:token/:user', function(request, response) {
-  chat.getChatByUser(request.params.user, request.params.token, function(chat) {
+router.get('/', function(req, res) {
+  res.render('index', { layout: null })
+})
+
+router.get('/chat/:token/:user', function(req, res) {
+  chat.getChatByUser(req.params.user, req.params.token, function(chat) {
     if (chat != null) {
-      response.render('chat', {
-        user: request.params.user,
-        token: request.params.token,
+      res.render('chat', {
+        user: req.params.user,
+        token: req.params.token,
         room: chat._id
       })
     } else {
-      response.render('error', {
+      res.render('error', {
         error: 'Nope'
       })
     }
